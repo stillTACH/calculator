711f29a0b396017bd4684ad257b55f3e3a35bed5
Tach <felixhanssel@aol.com>
2022-07-14 19:50:42 -0700

    Added sums, subtraction, and multiplication

--- main.py
+++ main.py
@@ -8,9 +8,27 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
+def sum(number1,number2):
+    return number1 + number2
+
+def diff(number1,number2):
+    return number1 - number2
+
+def multiply(number1,number2):
+    return number1 * number2
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    thesum = sum(5,2)
+    print(thesum)
+    thesum = sum(8,4)
+    print(thesum)
+    thediff = diff(3,5)
+    print(thediff)
+    print(sum(4,diff(3,8)))
+
+
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
